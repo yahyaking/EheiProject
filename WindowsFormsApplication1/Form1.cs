87cb338fbdd7050af15fa1d7baf9e46e4bc5488f
yahyaking <yahya-aouladchiba@hotmail.com>
2016-02-26 17:42:22 +0000

    change2

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -18,7 +18,7 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello world");
+            MessageBox.Show("Hello yahya");
         }
     }
 }
